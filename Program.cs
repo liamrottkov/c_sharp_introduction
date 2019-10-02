9f09510eaa08f4e197cce3b06e7b06ddaedee504
Liam Rottkov <liamrottkov@gmail.com>
2019-10-02 18:33:03 -0400

    finished project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// Namespace
+namespace ConsoleApp2
+{
+    //Main Class
+    class Program
+    {   
+        //Entry Point Method
+        static void Main(string[] args)
+        {
+            // Variables
+            string name = "Liam Rottkov";
+            int age = 24;
+
+           // Console.WriteLine("Hello "+name);
+            Console.WriteLine("{0} is {1}",name,age);
+        }
+    }
+}
